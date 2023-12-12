df96bb1f999dd0dfe4f8ae2bb90aaa728ea67c44
juliorm9 <juliorubiomorales9@gmail.com>
2023-12-12 18:28:21 +0100

    cambio42

--- src/main/java/es/upm/dit/apsv/gpsenabledtruckssimulator/GPSEnabledTrucksSimulatorApplication.java
+++ src/main/java/es/upm/dit/apsv/gpsenabledtruckssimulator/GPSEnabledTrucksSimulatorApplication.java
@@ -12,6 +12,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
